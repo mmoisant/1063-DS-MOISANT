fff3ee7205d65fd43ec100331eccbcbcfedb523c
mmoisant <marcusmoisant@yahoo.com>
2017-03-01 12:35:25 -0600

    Create homework-3.cpp

---
+++ Assignments/Homework3/homework-3.cpp
@@ -0,0 +1,34 @@
+ /**
+* Homework-3
+* Marcus Moisant
+* @Description: 
+*     
+* 1063 Data Structures
+* Spring 2017
+* 01 03 2017
+*/
+ 
+ 
+ /**
+* @FunctionName: Print
+* @Description: (of fix)
+*     FIX: FRONT AND REAR WERE BOTH EQUAL TO 0 & THE WOULD NOT PRINT. FIX BY USING isFull FUNCTION TO SEE IF QUEUE WAS FULL & KEEP PRINTING 
+* @Params:
+*    NONE
+* @Returns:
+*    NONE
+*/
+  //////FIX ME/////
+  void Print()
+  {
+  int Index = Front;
+  bool isFull = Full();
+
+  while(Index != Rear || isFull)
+  {
+    cout<<Q[Index]<<" ";
+    Index = ((Index + 1) % (ArraySize));
+    isFull = false;
+  }
+  cout<<endl;  
+}
